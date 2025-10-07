35c0e8f19c1f0bb27f45f2a90c3e36768c8b014c
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-07 12:06:17 +0530

    javalanguage.java

---
+++ Java.java
@@ -0,0 +1,9 @@
+// This is a basic Java program to print "Hello, World!" to the console.
+public class HelloWorld {
+
+    // The main method is the entry point of the program.
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
